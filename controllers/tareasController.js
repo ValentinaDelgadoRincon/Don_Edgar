b6babb088b29265861bd3a563de374d97f3efcef
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-09-19 03:46:54 -0500

    feat: :sparkles: create conection with mongo
    crete conection with mongo and the logica about interactive menu
    
    By@CamilaFlorez

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,24 +1,41 @@
 
 import inquirer from 'inquirer';
+import _ from "lodash";
 import { tareas } from '../data/tareas.js';
 import { conectar } from '../config/persistenciaArchivos.js';
 
 export async function agregarTarea() {
   try {
-    const { descripcion } = await inquirer.prompt([
-      { type: 'input', name: 'descripcion', message: 'Descripción de la tarea:' }
+    const { titulo, descripcion, prioridad } = await inquirer.prompt([
+      { type: "input", name: "titulo", message: "Título de la tarea:" },
+      { type: 'input', name: 'descripcion', message: 'Descripción de la tarea:' },
+      {
+        type: "list",
+        name: "prioridad",
+        message: "Prioridad:",
+        choices: ["alta", "media", "baja"],
+        default: "media"
+      }
     ]);
 
+    if (_.isEmpty(titulo.trim())) {
+      console.log(" El título no puede estar vacío.");
+      return;
+    }
     const nueva = {
       id: Date.now(),
+      titulo: titulo.trim(),
       descripcion: descripcion.trim(),
-      completada: false
+      prioridad,
+      estado: "pendiente"
     };
 
     const db = await conectar();
     const coleccion = db.collection("edgarEnLaNube");
     await coleccion.insertOne(nueva);
     tareas.push(nueva);
+
+    tareas.splice(0, tareas.length, ..._.uniqBy(tareas, "titulo"));
 
     console.log('✅ Tarea agregada.');
   } catch (error) {
@@ -28,145 +45,138 @@ export async function agregarTarea() {
 }
 
 export async function listarTareas() {
-  const db = await conectar();
-  const coleccion = db.collection("edgarEnLaNube");
-  const tareas = await coleccion.find().toArray();
-  if (tareas.length === 0) {
-    console.log('📭 No hay tareas registradas.');
+  try {
+    const db = await conectar();
+    const coleccion = db.collection("edgarEnLaNube");
+    const tareas = await coleccion.find().toArray();
+    if (_.isEmpty(tareas)) {
+      console.log('📭 No hay tareas registradas.');
+      return;
+    }
+    const ordenadas = _.orderBy(tareas, ["prioridad", "descripcion"], ["asc", "asc"]);
+    const agrupadas = _.groupBy(ordenadas, "estado");
 
-import _ from "lodash";
-import {tareas} from "../data/tareas.js";
+    console.log("\n📋 Pendientes:");
+    console.table(agrupadas["pendiente"] || []);
 
+    console.log("\n📋 En progreso:");
+    console.table(agrupadas["en progreso"] || []);
 
-function agregarTarea(titulo, descripcion, prioridad = "media") {
-  if (_.isEmpty(titulo.trim())) {
-    console.log("El título no puede estar vacío.");
-    return;
+    console.log("\n📋 Completadas:");
+    console.table(agrupadas["completada"] || []);
+
+  } catch (error) {
+    console.log(" Error al listar tareas:", error);
   }
-
-  const nuevaTarea = {
-    id: _.uniqueId("tarea_"),
-    titulo,
-    descripcion,
-    estado: "pendiente",
-    prioridad
-  };
-
-  tareas.push(nuevaTarea);
-
-
-tareas.splice(0, tareas.length, ..._.uniqBy(tareas, "titulo"));
-
-  console.log("Tarea agregada correctamente.");
-}
-
-function listarTareas() {
-  if (_.isEmpty(tareas)) {
-    console.log("No hay tareas registradas.");
-
-    return;
-  }
-
-
-  const ordenadas = _.orderBy(tareas, ["prioridad", "titulo"], ["asc", "asc"]);
-  const agrupadas = _.groupBy(ordenadas, "estado");
-
-  console.log("\nPendientes:");
-  console.table(agrupadas["pendiente"] || []);
-
-  console.log("\nEn progreso:");
-  console.table(agrupadas["en progreso"] || []);
-
-  console.log("\nCompletadas:");
-  console.table(agrupadas["completada"] || []);
 }
 
 
 export async function editarTarea() {
-  if (tareas.length === 0) return console.log('⚠️ No hay tareas para editar.');
+  try {
+    const db = await conectar();
+    const coleccion = db.collection("edgarEnLaNube");
+    const tareas = await coleccion.find().toArray();
 
-  const { indice } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'indice',
-      message: 'Selecciona una tarea para editar:',
-      choices: tareas.map((t, i) => ({
-        name: t.descripcion,
-        value: i
-      }))
-    }
-  ]);
+    if (_.isEmpty(tareas)) return console.log('⚠️ No hay tareas para editar.');
 
-  const { nuevaDescripcion } = await inquirer.prompt([
-    { type: 'input', name: 'nuevaDescripcion', message: 'Nueva descripción:' }
-  ]);
+    const { tareaId } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'tareaId',
+        message: 'Selecciona una tarea para editar:',
+        choices: tareas.map((t) => ({
+          name: t.descripcion,
+          value: t.id
+        }))
+      }
+    ]);
 
-  const db = await conectar();
-  const coleccion = db.collection("edgarEnLaNube");
-  await coleccion.updateOne(
-  {id:tareas[indice].id},
-  {$set:{descripcion:nuevaDescripcion.trim()}}
-  );
+    const { nuevaDescripcion } = await inquirer.prompt([
+      { type: 'input', name: 'nuevaDescripcion', message: 'Nueva descripción:' }
+    ]);
 
-  tareas[indice].descripcion = nuevaDescripcion.trim();
-  console.log('✏️ Tarea actualizada.');
+    await coleccion.updateOne(
+      { id: tareaId },
+      { $set: { descripcion: nuevaDescripcion.trim() } }
+    );
+
+    console.log('✏️ Tarea actualizada.');
+  } catch (error) {
+    console.log("Error al actualizar la tarea...", error);
+  }
+
 }
 
 export async function eliminarTarea() {
-  if (tareas.length === 0) return console.log('⚠️ No hay tareas para eliminar.');
+  try {
+    const db = await conectar();
+    const coleccion = db.collection("edgarEnLaNube");
+    const tareas = await coleccion.find().toArray();
 
-  const { indice } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'indice',
-      message: 'Selecciona una tarea para eliminar:',
-      choices: tareas.map((t, i) => ({
-        name: t.descripcion,
-        value: i
-      }))
+    if (_.isEmpty(tareas)) return console.log('⚠️ No hay tareas para eliminar.');
+
+    const { tareaId } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'tareaId',
+        message: 'Selecciona una tarea para eliminar:',
+        choices: tareas.map((t) => ({
+          name: t.descripcion,
+          value: t.id
+        }))
+      }
+    ]);
+
+    await coleccion.deleteOne({ id: tareaId })
+
+    console.log('🗑️ Tarea eliminada.');
+  } catch (error) {
+    console.log("Error al eliminar tarea", error)
+  }
+
+}
+
+export async function cambiarEstado(id, nuevoEstado) {
+  try {
+    const db = await conectar();
+    const coleccion = db.collection("edgarEnLaNube");
+    const tarea = await coleccion.findOne({ id });
+
+    if (_.isEmpty(tarea)) {
+      console.log("Tarea no encontrada");
+      return;
+    };
+
+    await coleccion.updateOne(
+      { id },
+      { $set: { estado: nuevoEstado } }
+    )
+    console.log("Estado actualizado correctamente.");
+  } catch (error) {
+    console.log("Error al cambiar estado", error)
+  }
+}
+
+
+export async function buscarTareas(palabraClave) {
+  try {
+    const db = await conectar();
+    const coleccion = db.collection("edgarEnLaNube");
+    const tareas = await coleccion.find({
+      $or: [
+        { titulo: { $regex: palabraClave, $options: "i" } },
+        { descripcion: { $regex: palabraClave, $options: "i" } }
+      ]
+    }).toArray();
+
+    if (_.isEmpty(tareas)) {
+      console.log("No se encontraron coincidencias");
+      return;
     }
-  ]);
-
-  const tareaEliminada = tareas[indice];
-
-  const db = await conectar();
-  const coleccion = db.collection("edgarEnLaNube");
-  await coleccion.deleteOne({
-    id:tareaEliminada.id
-  })
-
-  tareas.splice(indice, 1);
-  console.log('🗑️ Tarea eliminada.');
-
-
-function cambiarEstado(id, nuevoEstado) {
-  const tarea = _.find(tareas, { id });
-  if (!tarea) {
-    console.log("Tarea no encontrada.");
-    return;
+    console.log(" Resultados de la búsqueda:");
+    console.table(tareas);
+  } catch (error) {
+    console.log(" Error al buscar tarea", error);
   }
-  tarea.estado = nuevoEstado;
-  console.log("Estado actualizado correctamente.");
 }
 
-
-function buscarTareas(palabraClave) {
-  const resultados = _.filter(tareas, (t) =>
-    _.includes(t.titulo.toLowerCase(), palabraClave.toLowerCase())
-  );
-
-  if (_.isEmpty(resultados)) {
-    console.log("No se encontraron coincidencias.");
-    return;
-  }
-
-  console.log("Resultados de la búsqueda:");
-  console.table(resultados);
-}
-
-export {
-  agregarTarea,
-  listarTareas,
-  cambiarEstado,
-  buscarTareas
-};
--- index.js
+++ index.js
@@ -1,59 +1,44 @@
-import readline from "readline";
-import { agregarTarea,listarTareas,cambiarEstado,buscarTareas } from "./controllers/tareasController.js";
+import inquirer from "inquirer";
+import { agregarTarea, listarTareas, cambiarEstado, buscarTareas, editarTarea, eliminarTarea } from "./controllers/tareasController.js";
 import { mostrarMenu } from "./utils/menu.js";
-// const {
-//   agregarTarea,
-//   listarTareas,
-//   cambiarEstado,
-//   buscarTareas
-// } = require("./controllers/tareasController");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function iniciar() {
+async function iniciar() {
   mostrarMenu();
-  rl.question("selecciona una opción: ", (opcion) => {
-    switch (opcion) {
-      case "1":
-        rl.question("Título de la tarea: ", (titulo) => {
-          rl.question("Descripción: ", (descripcion) => {
-            rl.question("Prioridad (alta, media, baja): ", (prioridad) => {
-              agregarTarea(titulo, descripcion, prioridad);
-              iniciar();
-            });
-          });
-        });
-        break;
-      case "2":
-        listarTareas();
-        iniciar();
-        break;
-      case "3":
-        rl.question("ID de la tarea: ", (id) => {
-          rl.question("Nuevo estado (pendiente, en progreso, completada): ", (estado) => {
-            cambiarEstado(id, estado);
-            iniciar();
-          });
-        });
-        break;
-      case "4":
-        rl.question("Palabra clave a buscar: ", (palabra) => {
-          buscarTareas(palabra);
-          iniciar();
-        });
-        break;
-      case "5":
-        console.log("Saliendo del sistema...");
-        rl.close();
-        break;
-      default:
-        console.log("Opción no válida.");
-        iniciar();
-    }
-  });
+  const { opcion } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'opcion',
+      message: 'Ingresa una opcion:',
+      choices: [
+        { name: "1. Agregar tarea", value: "1" },
+        { name: "2. Listar tareas", value: "2" },
+        { name: "3. Editar tarea", value: "3" },
+        { name: "4. Eliminar tarea", value: "4" },
+        { name: "5. Salir", value: "5" },
+      ],
+    },
+  ]);
+  switch (opcion) {
+    case "1":
+      await agregarTarea();
+      break;
+    case "2":
+      await listarTareas();
+      break;
+    case "3":
+      await editarTarea();
+      break;
+    case "4":
+      await eliminarTarea();
+      break
+    case "5":
+      console.log("Saliendo del programa...");
+      return;
+    default:
+      console.log("Opcion inválida")
+  }
+
+  await iniciar();
 }
 
 iniciar();
