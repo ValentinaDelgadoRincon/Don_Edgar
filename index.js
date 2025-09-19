d9f54369d2e6a0caaf89f8afa6a99e1576ff8a78
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-09-19 09:59:55 -0500

    feat: :sparkles: close conectio with db
    close conection with mongodb
    
    By@CamilaFlorez

--- index.js
+++ index.js
@@ -8,72 +8,78 @@ import {
   eliminarTarea,
 } from "./controllers/tareasController.js";
 import { mostrarMenu } from "./utils/menu.js";
+import { cerrarConexion } from "./config/persistenciaArchivos.js";
 
 async function iniciar() {
-  mostrarMenu();
+  let salir = false;
+  while (!"6") {
+    mostrarMenu();
 
-  const { opcion } = await inquirer.prompt([
-    {
-      type: "list",
-      name: "opcion",
-      message: "Ingresa una opción:",
-      choices: [
-        { name: "1. Agregar tarea", value: "1" },
-        { name: "2. Listar tareas", value: "2" },
-        { name: "3. Marcar tarea como completada", value: "3" },
-        { name: "4. Editar tarea", value: "4" },
-        { name: "5. Eliminar tarea", value: "5" },
-        { name: "6. Salir", value: "6" },
-      ],
-    },
-  ]);
+    const { opcion } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "opcion",
+        message: "Ingresa una opción:",
+        choices: [
+          { name: "1. Agregar tarea", value: "1" },
+          { name: "2. Listar tareas", value: "2" },
+          { name: "3. Marcar tarea como completada", value: "3" },
+          { name: "4. Editar tarea", value: "4" },
+          { name: "5. Eliminar tarea", value: "5" },
+          { name: "6. Salir", value: "6" },
+        ],
+      },
+    ]);
 
-  switch (opcion) {
-    case "1":
-      await agregarTarea();
-      break;
+    switch (opcion) {
+      case "1":
+        await agregarTarea();
+        break;
 
-    case "2":
-      const { filtro } = await inquirer.prompt([
-        {
-          type: "list",
-          name: "filtro",
-          message: "¿Qué tareas quieres listar?",
-          choices: [
-            { name: "Todas", value: "todas" },
-            { name: "Completadas", value: "completadas" },
-            { name: "Pendientes", value: "pendientes" },
-          ],
-        },
-      ]);
-      if (filtro === "todas") {
-        await listarTareas();
-      } else {
-        await listarPorEstado(filtro);
-      }
-      break;
+      case "2":
+        const { filtro } = await inquirer.prompt([
+          {
+            type: "list",
+            name: "filtro",
+            message: "¿Qué tareas quieres listar?",
+            choices: [
+              { name: "Todas", value: "todas" },
+              { name: "Completadas", value: "completadas" },
+              { name: "Pendientes", value: "pendientes" },
+            ],
+          },
+        ]);
+        if (filtro === "todas") {
+          await listarTareas();
+        } else {
+          await listarPorEstado(filtro);
+        }
+        break;
 
-    case "3":
-      await cambiarEstado();
-      break;
+      case "3":
+        await cambiarEstado();
+        break;
 
-    case "4":
-      await editarTarea();
-      break;
+      case "4":
+        await editarTarea();
+        break;
 
-    case "5":
-      await eliminarTarea();
-      break;
+      case "5":
+        await eliminarTarea();
+        break;
 
-    case "6":
-      console.log("Saliendo del programa...");
-      return;
+      case "6":
+        salir = true;
+        console.log("Saliendo del programa...");
+        await cerrarConexion();
+        break
+      default:
+        console.log("Opción inválida");
+    }
 
-    default:
-      console.log("Opción inválida");
+    await iniciar();
   }
-
-  await iniciar();
 }
+
 
 iniciar();
